b936eb5c9b755f7157d5221eb058bcfa27e4cb4b
Edu4rd02 <josebecerra.0870@gmail.com>
2026-05-18 19:48:38 -0700

    Logic Unit Test Done

--- app/src/test/java/com/example/todoappch7/todoapp/TodoViewModelTest.kt
+++ app/src/test/java/com/example/todoappch7/todoapp/TodoViewModelTest.kt
@@ -2,6 +2,7 @@ package com.example.todoappch7.todoapp
 
 import com.example.todoappch7.TodoViewModel
 import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertFalse
 import junit.framework.TestCase.assertTrue
 import org.junit.Before
 import org.junit.Test
@@ -25,5 +26,63 @@ class TodoViewModelTest {
         // Assert -- Verify the result
         assertEquals(1,viewModel.getTaskCount())
         assertTrue(viewModel.containsTask("Buy Groceries"))
+    }
+
+    @Test
+    fun add_Task_withBlankTitle_isIgnored(){
+        // Action
+        viewModel.addTask("              ")
+
+        // Assert
+        assertEquals(0,viewModel.getTaskCount())
+        assertFalse(viewModel.containsTask("              "))
+    }
+
+    @Test
+    fun add_Task_withEmptyString_isIgnored(){
+        // Action
+        viewModel.addTask("")
+
+        // Assert
+        assertEquals(0,viewModel.getTaskCount())
+        assertFalse(viewModel.containsTask(""))
+    }
+
+    @Test
+    fun add_Task_tittleIsTrimmed(){
+        // Action
+        viewModel.addTask("     Do exercise      ")
+
+        // Assert
+        assertEquals(1,viewModel.getTaskCount())
+        assertTrue(viewModel.containsTask("Do exercise"))
+    }
+
+    @Test
+    fun removeTask_withValidId_removesTask(){
+        // Arrange - add a task first so we have something to remove
+        viewModel.addTask("Do exercise")
+        val taskId = viewModel.task[0].id
+
+        // Action
+        viewModel.removeTask(taskId)
+
+        // Assert
+        assertEquals(0,viewModel.getTaskCount())
+        assertFalse(viewModel.containsTask("Do exercise"))
+    }
+
+    // Challenge
+    @Test
+    fun removeTask_withInvalidId_doesNothing(){
+        // Arrange
+        viewModel.addTask("Eat")
+
+        // Action
+        viewModel.removeTask(99)
+
+        // Assert
+        assertEquals(1,viewModel.getTaskCount())
+        assertTrue(viewModel.containsTask("Eat"))
     }
 }
